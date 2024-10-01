98fca719ee59af6943ec7cd6a125b7e82bf5417a
michaelrzg <michaelrizig@gmail.com>
2024-10-01 14:36:35 -0400

    added main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Logistical Regression for Sentiment Analysis of Amazon Reviews.
+# Michael Rizig
+
+
+def preprocess(textstring):
+    """Preprocesses a string by removing stop words, symbols, digits, etc.
+    outputs a list of tokens."""
+    pass
+def logisticalRegression(sample):
+    """logreg function via sigmoid to return a value between 0-1
+    representing the probability of a given class.
+    If output > .5, it returns class 1,
+    else output <= .5 and returns class 0.
+    :input is a sample"""
+    pass
