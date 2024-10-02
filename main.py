564ca97dd91cf3eb65fbc8b37f4702edf7c203ef
michaelrzg <michaelrizig@gmail.com>
2024-10-01 22:39:06 -0400

    added feature extraction, extracted features to files

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ import math # for implementing sigmoid
 import threading # for efficientcy
 import concurrent.futures # for thread management
 
+from preprocess_dataset import preprocess
 
 def initilize():
     """Initilizees the model by downloading stopwords list, loading data, and preprocessing datasetsS"""
     download_stopwords()
     data = load_data()
-
+    # if data is not preprocessed uncomment below (this will take time)
+    # preprocess()
+    # if features are not extracted uncomment below
+    # extract_features(data[2])
+    # extract_features(data[3], False)
     return data
 
 
@@ -38,6 +43,7 @@ def load_data():
         positive_words = []
         for line in positive:
             line = line.replace('\n','')
+            line = line.lower()
             positive_words.append(line)
         
         # load negative words into array
@@ -45,22 +51,25 @@ def load_data():
         negative_words = []
         for line in negative:
             line = line.replace('\n','')
+            line = line.lower()
             negative_words.append(line)
         
         # load testing dataset into array of tuples [data:string, label:int]
-        test = open("dataset/test_amazon.csv")
+        test = open("dataset/test_formatted.csv")
         test_set = []
         for line in test:
             x = line.split(",")
             x[1] = x[1].replace("\n","")
+            x[1] = x[1].lower()
             test_set.append([x[1],int(x[0])])
         
         # load training dataset into array of tuples [data:string, label:int]
-        training = open("dataset/train_amazon.csv")
+        training = open("dataset/train_formatted.csv")
         train_set = []
         for line in training:
             x = line.split(",")
             x[1] = x[1].replace("\n","")
+            x[1] = x[1].lower()
             train_set.append([x[1],int(x[0])])
     except FileNotFoundError:
         print("one or more files not found. Check paths")
@@ -83,10 +92,10 @@ def remove_stopwords(textstring, outputs,i):
 
 
 
-def extract_features():
+def extract_features(dataset, training=True):
     """ Takes in set of tokens and returns feature set
     
-    output X = [x1,x2,x3,x4,x5]
+    output X = [x1,x2,x3,x4,x5, c]
     
     x1 = # of positive lexicons 
 
@@ -94,9 +103,25 @@ def extract_features():
 
     x3 = if no ∈ sample (either 0 or 1 value)
 
-    x4 = log(word count) 
+    x4 = ∃ '!' ∈ sample (either 0 or 1)
+
+    x5 = log(word count) 
     
-    x5 = tbd"""
+    c = class (1 = positive class 0 = negative class)
+    """
+    if training:
+        out = open("dataset/training_features.csv",'w')
+    else:
+        out = open("dataset/testing_features.csv", 'w')
+    for sample in dataset:
+        x1 = len([x for x in sample[0].split(" ") if positive_words.count(x)>0])
+        x2 =len([x for x in sample[0].split(" ") if negative_words.count(x)>0])
+        x3 = 1 if sample[0].count("no")>0 else 0
+        x4 = 1 if sample[0].count("!")>0 else 0
+        x5 = math.log(len(sample[0].split(" ")),10)
+        out.write(f"{x1},{x2},{x3},{x4},{x5},{sample[1]}\n")
+        
+
 
     pass
 
@@ -112,4 +137,12 @@ def logistical_regression(sample):
     pass
 
 data = initilize()
+positive_words  = data[0]
+negative_words = data[1]
+training_set = data[2]
+testing_set = data[3]
+
+extract_features(training_set)
+print("half")
+extract_features(testing_set, False)
 
--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -1,53 +1,52 @@
 import nltk
+class preprocess:
+    def remove_stopwords(textstring):
+        """Preprocesses a string by removing stop words, symbols, digits, etc.
+        outputs a list of tokens."""
+        
+        # assert that a string was passed
+        assert isinstance(textstring,str) , "THIS IS NOT A STRING"
+        # parse string into array of words
+        words = textstring.split(" ")
+        # remove stopwords
+        words = [word for word in words if word not in nltk.corpus.stopwords.words('english')]
+        return words
 
-def remove_stopwords(textstring):
-    """Preprocesses a string by removing stop words, symbols, digits, etc.
-    outputs a list of tokens."""
-    
-    # assert that a string was passed
-    assert isinstance(textstring,str) , "THIS IS NOT A STRING"
-    # parse string into array of words
-    words = textstring.split(" ")
-    # remove stopwords
-    words = [word for word in words if word not in nltk.corpus.stopwords.words('english')]
-    return words
 
+    def preprocess_dataset(self):
+        """Preprocess dataset by removing stopwords"""
+        # create output files
+        test_data_output_file = open("dataset/test_formatted.csv", mode="x")
+        train_data_output_file = open("dataset/train_formatted.csv", mode="x")
+        # read raw data
+        test_data_raw = open("dataset/test_amazon.csv")
+        train_data_raw = open("dataset/train_amazon.csv")
 
-def preprocess_dataset():
-    """Preprocess dataset by removing stopwords"""
-    # create output files
-    test_data_output_file = open("dataset/test_formatted.csv", mode="x")
-    train_data_output_file = open("dataset/train_formatted.csv", mode="x")
-    # read raw data
-    test_data_raw = open("dataset/test_amazon.csv")
-    train_data_raw = open("dataset/train_amazon.csv")
-
-    for line in test_data_raw:
-        addnewline =True
-        line = line.split(",")
-        rmstop = remove_stopwords(line[1])
-        test_data_output_file.write(f"{line[0]}, ")
-        for x in rmstop:
-            x.replace(",","")
-            if x.count("\n")>0:
-                 addnewline = False
-            test_data_output_file.write(f"{x} ")
-        if(addnewline):
-            test_data_output_file.write("\n")
-    for line in train_data_raw:
-        addnewline=True
-        line = line.split(",")
-        rmstop = remove_stopwords(line[1])
-        train_data_output_file.write(f"{line[0]}, ")
-        for x in rmstop:
-            x.replace(",","")
-            if x.count("\n")>0:
-                 addnewline = False
-            train_data_output_file.write(f"{x} ")
-        if(addnewline):
-            train_data_output_file.write("\n")
+        for line in test_data_raw:
+            addnewline =True
+            line = line.split(",")
+            rmstop = self.remove_stopwords(line[1])
+            test_data_output_file.write(f"{line[0]}, ")
+            for x in rmstop:
+                x.replace(",","")
+                if x.count("\n")>0:
+                    addnewline = False
+                test_data_output_file.write(f"{x} ")
+            if(addnewline):
+                test_data_output_file.write("\n")
+        for line in train_data_raw:
+            addnewline=True
+            line = line.split(",")
+            rmstop = self.remove_stopwords(line[1])
+            train_data_output_file.write(f"{line[0]}, ")
+            for x in rmstop:
+                x.replace(",","")
+                if x.count("\n")>0:
+                    addnewline = False
+                train_data_output_file.write(f"{x} ")
+            if(addnewline):
+                train_data_output_file.write("\n")
             
 
-preprocess_dataset()
 
 
